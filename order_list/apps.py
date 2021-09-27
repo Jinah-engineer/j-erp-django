e2ca6410d83020928154f8d1d365a7bcaede0522
Jinah-engineer <jeeeengineer@gmail.com>
2021-09-27 14:09:47 +0900

    project structure

--- order_list/apps.py
+++ order_list/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DeliveryManagementConfig(AppConfig):
+class OrderListConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'delivery_management'
+    name = 'order_list'
--- order_sheet/apps.py
+++ order_sheet/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OrderManagementConfig(AppConfig):
+class OrderSheetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'order_management'
+    name = 'order_sheet'
