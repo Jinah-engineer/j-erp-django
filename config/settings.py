d64e10cfc8b60ceb20e16f7f3fed86e378d3d47d
MF <kstydevil@gmail.com>
2021-09-27 16:36:08 +0900

    pycharm test

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
+    'accounts',
     'order_list',
     'order_sheet',
     'information',
