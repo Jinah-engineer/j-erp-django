ca91e6eb4e5314f5b21411f297beb6f2fb90c72b
MF1204 <kstydevil@gmail.com>
2021-10-01 17:09:28 +0900

    information model 추가
    accounts
    - 로그인기능 추가(테스트용 id : ggu / pw : 12#$)
    - 로그인 검증 통과, 성공시 주문리스트 페이지로 리다이렉트
    
    config settings
    - 맨 밑줄 LOGIN_REDIRECT_URL
    - 데이터베이스 : 개발용 DB(서버컴퓨터) 설정
    - INSTALLED_APPS : 폰트머시기 삭제(추가해두니 runserver가 안되어서 지웠습니다)
    
    templates
    - accounts 디렉토리 추가 : app내부 템플릿 디렉토리로 경로설정을 못해서 만듦
    - navigation.html 수정 : 미홍로고 이미지 추가
    - base.html 수정 : 로그인페이지 진입버튼 추가(테스트용)

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,6 @@
-
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls import url
 from accounts import views
 from order_sheet import views
 from order_list import views
