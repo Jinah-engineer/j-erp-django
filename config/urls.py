7e8f1a24a3efe669ab4038d1171a8493dcb523a1
MF1204 <kstydevil@gmail.com>
2021-10-14 00:00:31 +0900

    20211013 - auth complete

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('order_list.urls')),
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('sheet/', include('order_sheet.urls')),
--- order_list/urls.py
+++ order_list/urls.py
@@ -6,6 +6,7 @@ from order_list.views import *
 app_name = "list"
 
 urlpatterns = [
+    path('', order_list_view, name='home'),
     path('orderlist/', order_list_view, name='orderlist'),
     path('orderdetail/', order_detail_view, name='orderdetail'),
 ]
