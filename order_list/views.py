ef1010c538eb89718af14442ba549b3e71df364c
Jinah-engineer <jeeeengineer@gmail.com>
2021-10-21 16:10:36 +0900

    박진아 push push

--- order_list/views.py
+++ order_list/views.py
@@ -12,50 +12,6 @@ from django.utils import timezone
 from information.models import Product, Delivery, Pay_type, Order_type
 from order_list.models import Order
 
-
-
-'''
-    구현 list
-
-        1. 검색바 (search - module)
-            1) 달력 API
-            2) pagination 연동
-            3) Ajax 화면 전환 버튼 (제작 대상, 출고 대상, 출고 완료 목록)
-            4) 검색기능
-        2. 주문 목록 인쇄 (PrintOrderList)
-        3. Excel 주문 업로드 (UploadOrderList)
-        4. Excel 주문 다운로드 (DownloadOrderList)
-        5. 개별 주문장 출력 (PrintOrderSample)
-        6. 주문 수정 (ModifyOrder)
-        7. 주문 취소 (UndoOrder)
-        8. 상태 변경 (ChangeOrderStatus)
-            1) 주문 완료 (order_completed)
-            2) 제작 완료 (produce_completed)
-            3) 출고 완료 (release_completed)
-        9. 정렬 (SortOrderList)
-            1) 10개씩
-            2) 50개씩
-            3) 100개씩
-        11. 페이지네이션 (pagination)
-'''
-
-# print('---------here---------')
-#
-# pno = request.GET['pno']
-# print(pno)
-# print(type(pno))
-#
-# if request.is_ajax():
-#
-#     data = {
-#
-#         }
-#     print(data)
-#     print(type(data))
-#
-#     return JsonResponse(data)
-
-
 # ====================== 전체 주문 목록
 def order_list_view(request):
 
@@ -88,6 +44,15 @@ def order_list_view(request):
         "production_completed": production_completed,
         "release_completed": release_completed,
     }
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        context = {
+
+        }
+        return JsonResponse(context)
+
+
     return render(request, "list.html", context)
 
 
@@ -104,6 +69,22 @@ def order_detail_view(request):
         print(type(data))
 
         return JsonResponse(data, content_type="application/json")
+
+
+# ====================== 목록 변경
+# def change_list_view(request):
+#
+#     order_completed_list = Order.object.all().filter(state="주문완료").order_by('-pno')
+#     production_completed_list = Order.object.all().filter(state="제작완료").order_by('-pno')
+#     release_completed_list = Order.object.all().filter(state="출고완료").order_by('-pno')
+#
+#     data = {
+#         "order_completed_list": order_completed_list,
+#         "production_completed_list": production_completed_list,
+#         "release_completed_list": release_completed_list,
+#     }
+#
+#     return JsonResponse(data, content_type="application/json")
 
 
 
